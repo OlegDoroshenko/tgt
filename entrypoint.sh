d5944151c9667b5de2eb4966a6b6dc1e1ff18fd0
Oleg Kristal <oleg@doroshenko.su>
2024-09-24 14:02:46 +0300

    Update configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#VOLPATH="/dev/zvol/rpool"
+#IQN="iqn.2024-09.ru.vmhouse.iscsi"
 
 tgtd -f &> tgt.log &
 tgtadm --op update --mode sys --name State -v offline
 tgt-admin -e -c /etc/tgt/targets.conf
 tgtadm --op update --mode sys --name State -v ready
-tail -f tgt.log
+
+for VOL in `ls -1 $VOLPATH`; 
+do
+  tgtadm --lld iscsi --op new --mode target --tid ${VOL: -2} --targetname $IQN:$VOL
+  tgtadm --lld iscsi --op new --mode logicalunit --tid ${VOL: -2} --lun 1 --backing-store $VOLPATH/$VOL
+done
+echo ****************************************************************************
+tgtadm --lld iscsi --op show --mode target
+tail -100 -f tgt.log
